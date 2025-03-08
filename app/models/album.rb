85a49247c33975aca34ede9969e329693e8f579b
Lazaroenrico <lazaro.enr.menezes@gmail.com>
2025-03-08 17:35:45 -0300

    addition of endpoints categories, dashboar and search

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,7 +3,7 @@ class Album < ApplicationRecord
   belongs_to :artist
 
   has_many :songs
-  validates :tilte, presence: true
+  validates :title, presence: true
   validates :date, presence: true
 
   has_one_attached :cover
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,7 @@
 class Artist < ApplicationRecord
   has_many :albums
 
-  validate :name, presence: true
+  validates :name, presence: true
 
   has_one_attached :photo
 end
